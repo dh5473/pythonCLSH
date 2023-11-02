61fd698d2d94922f6c6f30dae2273ca50e778429
dh5473 <dh5473@naver.com>
2023-11-03 01:00:01 +0900

    Feat: error handling related to hostfile option

--- clsh/clsh.py
+++ clsh/clsh.py
@@ -17,24 +17,42 @@ def main(
     out: Annotated[str, typer.Option()] = "",
     err: Annotated[str, typer.Option()] = "",
 ):
-    
-    if hostfile:
+    # EAFP: Easier to Ask Forgiveness than Permission
+    try:
         with open(hostfile, 'r') as file:
             hosts = file.read().splitlines()
-        
-        ssh_nodes = connection.SSHconnection(hosts)
-        ssh_nodes.send_command(ctx.args)
-        
+    except:
+        try:
+            hosts_env = os.environ['CLSH_HOSTS']
+            hosts = list(hosts_env.split(":"))
+
+        except:
+            try:
+                hostfile_env = os.environ['CLSH_HOSTFILE']
+                
+                if not os.path.isabs(hostfile_env):
+                    cwd = os.getcwd()
+                    hostfile_env = os.path.abspath(os.path.join(cwd, hostfile_env))
+
+                with open(hostfile_env, 'r') as file:
+                    hosts = file.read().splitlines()
+
+            except:
+                try:
+                    with open("./hostfile", 'r') as file:
+                        hosts = file.read().splitlines()
+
+                except:
+                    print("--hostfile option is not provided. Please provide a hostfile.")
+                    exit(1)
+
+    if i:
+        pass
+
     else:
-        if os.environ.get('CLSH_HOSTS'):
-            pass
-        elif os.environ.get('CLSH_HOSTFILE'):
-            pass
-        elif os.path.isfile(hostfile):
-            pass
-        else:
-            print("--hostfile option is not provided. Please provide a hostfile.")
-            exit(1)
+        ssh_nodes = connection.SSHconnector(hosts)
+        ssh_nodes.excute_ssh_process(ctx.args)
+
 
     typer.echo(f"-i: {i}")
     typer.echo(f"--out: {out}")
