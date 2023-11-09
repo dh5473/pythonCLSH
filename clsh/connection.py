5cd3fd1e39367a4035a76954f322f442f1ae0c95
dh5473 <dh5473@naver.com>
2023-11-10 00:56:32 +0900

    Feat: interactive mode

--- clsh/connection.py
+++ clsh/connection.py
@@ -2,16 +2,18 @@ import subprocess
 import multiprocessing
 
 
-class SSHconnector:
+class SSHConnector:
     def __init__(self, hosts) -> None:
         self.hosts = hosts
+        self.processes = dict()
 
     def excute_ssh_process(self, cmd):
         def send_command(host):
             ssh_cmd = f"ssh {host} {cmd}"
             process = subprocess.Popen(
                                         ssh_cmd, 
-                                        shell=True, 
+                                        shell=True,
+                                        stdin=subprocess.PIPE,
                                         stdout=subprocess.PIPE,
                                         stderr=subprocess.PIPE,
                                         encoding='utf-8'
@@ -21,12 +23,42 @@ class SSHconnector:
             if output:
                 print(f"Output from {host}:")
                 print(output.strip())
+            # process.poll()
+        
+        def resend_command(host):
+            process = self.processes[host]
+            process.stdin.write(f"ssh {host} {cmd}\n")
+            process.stdin.flush()
 
-        processes = []
+            process.wait()
+            output, _ = process.communicate()
+            if output:
+                print(f"Output from {host}:")
+                print(output.strip())
+
         for host in self.hosts:
-            process = multiprocessing.Process(target=send_command, args=(host,))
-            processes.append(process)
+            if self.processes.get(host):
+                process = multiprocessing.Process(target=resend_command, args=(host,))
+            else:
+                process = multiprocessing.Process(target=send_command, args=(host,))
+                self.processes[host] = process
             process.start()
 
-        for process in processes:
+        for host, process in self.processes.items():
             process.join()
+        
+    def recv_commands(self):
+        while True:
+            cmd = input("clsh> ")
+            
+            if cmd == 'quit':
+                self.terminate_processes()
+                break
+            
+            self.excute_ssh_process(cmd)
+
+        self.terminate_processes()
+
+    def terminate_processes(self):
+        for host, process in self.processes.items():
+            process.terminate()
